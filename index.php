bd68a439f0290a9dc079168d5758318588f52aa5
Wopaz <wopaz@live.fr>
2014-11-23 13:21:46 +0100

    Update
    Rajout du squelette & header.

--- index.php
+++ index.php
@@ -3,6 +3,26 @@
 	require 'partials/header.php'; 
 ?>
 
+<!DOCTYPE html>
 
+<html>
+	<head>
+		<title>YoursProjects | Accueil</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	
+	<body>
+		<header>
+			<nav>
+				<ul>
+					<li>Accueil</li>
+					<li>Créer un compte</li>
+					<li style="margin-right: 20px;">Connexion</li>
+				</ul>
+			</nav>
+		</header>
+	</body>
+</html>
 
 <?php require 'partials/footer.php'; ?>
